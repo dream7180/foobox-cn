9c3b4335e0331ca552bbb3f236aa562a63cb42b8
Jack Lee <mikumikuer@gmail.com>
2025-11-19 02:25:19 +0800

    根据边框样式自动确定是否使用皮肤内置标题栏

--- script/js_panels/base.js
+++ script/js_panels/base.js
@@ -198,10 +198,16 @@ function detect_video() {
 }
 
 function set_panel() {
-	var ph = win_y - topbarh;
+    var ph = win_y, y_offset = 0;
+    if (UIHacks?.FrameStyle == 3) {
+        ph -= topbarh;
+        y_offset = topbarh;
+    } else if (UIHacks?.MainMenuState != 0) {
+        setTimeout(function() {UIHacks.MainMenuState = 0;}, 100);
+    }
 	try{
 		if(active_p.Width != ww || active_p.Height != ph)
-		active_p.Move(0, topbarh, ww, ph);
+		active_p.Move(0, y_offset, ww, ph);
 	}catch(e){}
 }
 
